3cf6f0af990746e89d35b9a27f3867140f623356
dellacross <victordella652@gmail.com>
2023-10-09 18:16:38 -0300

    Fix Warnings

--- src/components/Resume/index.jsx
+++ src/components/Resume/index.jsx
@@ -25,9 +25,9 @@ const Resume = () => {
           <h3>Victor Della Croce Maltez</h3>
           <h4>Graduando em Sistemas de Informação</h4>
           <div className="resume-medias-box">
-            <a className="linkedin-icon" href="https://www.linkedin.com/in/victor-della-croce-maltez-48711b187/" target="_blank"><ion-icon name="logo-linkedin"></ion-icon></a>
-            <a className="github-icon" href="https://github.com/dellacross" target="_blank"><ion-icon name="logo-github"></ion-icon></a>
-            <a className="insta-icon" href="https://www.instagram.com/victordella/" target="_blank"><ion-icon name="logo-instagram"></ion-icon></a>
+            <a className="linkedin-icon" href="https://www.linkedin.com/in/victor-della-croce-maltez-48711b187/" target="_blank"  rel="noreferrer"><ion-icon name="logo-linkedin"></ion-icon></a>
+            <a className="github-icon" href="https://github.com/dellacross" target="_blank"  rel="noreferrer"><ion-icon name="logo-github"></ion-icon></a>
+            <a className="insta-icon" href="https://www.instagram.com/victordella/" target="_blank"  rel="noreferrer"><ion-icon name="logo-instagram"></ion-icon></a>
           </div>
           <div className="curriculum-box">
             <button onClick={handleDownload}>
