8bf060a0f8c78efdcf3c72c22a1e100fc442dcfd
dellacross <victordella652@gmail.com>
2023-10-10 13:47:25 -0300

    Fix Warnings

--- src/components/AboutMe/index.jsx
+++ src/components/AboutMe/index.jsx
@@ -17,9 +17,9 @@ const AboutMe = () => {
             ))
           }
           <div className="resume-medias-box">
-            <a className="linkedin-icon" href="https://www.linkedin.com/in/victor-della-croce-maltez-48711b187/" target="_blank"><ion-icon name="logo-linkedin"></ion-icon></a>
-            <a className="github-icon" href="https://github.com/dellacross" target="_blank"><ion-icon name="logo-github"></ion-icon></a>
-            <a className="insta-icon" href="https://www.instagram.com/victordella/" target="_blank"><ion-icon name="logo-instagram"></ion-icon></a>
+            <a className="linkedin-icon" href="https://www.linkedin.com/in/victor-della-croce-maltez-48711b187/" target="_blank" rel="noreferrer"><ion-icon name="logo-linkedin"></ion-icon></a>
+            <a className="github-icon" href="https://github.com/dellacross" target="_blank" rel="noreferrer"><ion-icon name="logo-github"></ion-icon></a>
+            <a className="insta-icon" href="https://www.instagram.com/victordella/" target="_blank" rel="noreferrer"><ion-icon name="logo-instagram"></ion-icon></a>
           </div>
         </div>
       </div>
